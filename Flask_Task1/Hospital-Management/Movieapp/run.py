7c93c2a7bb77342b2256f562ebcfa5faeda97654
AJMECH94 <pawarajinkya97@gmail.com>
2022-10-20 16:21:51 +0530

    flask task

--- Flask_Task1/Hospital-Management/Movieapp/run.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-app.config['SECRET_KEY'] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
-app.config['SESSION_TYPE'] = 'memcached'
-app.run()
--- Flask_Task1/Hospital-Management/resources/routes.py
+++
@@ -1,9 +0,0 @@
-from .auth import SignupApi, LoginApi
-from .reset_password import ForgotPassword, ResetPassword
-
-
-def initialize_routes(api):
-    api.add_resource(SignupApi, '/api/auth/signup')
-    api.add_resource(LoginApi, '/api/auth/login')
-    api.add_resource(ForgotPassword, '/api/auth/forget')
-    api.add_resource(ResetPassword, '/api/auth/reset')
